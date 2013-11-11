1eb0d56dbf92b9d6e362e8bcbeb502c9d7afb507
bit <bit@bit-PC>
2013-11-11 10:57:21 +0900

    hhh

--- safeTaxi/src/main/java/soup/taxi/control/HomeController.java
+++ safeTaxi/src/main/java/soup/taxi/control/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Test home! The client locale is {}.", locale);
+		logger.info("hhh home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
